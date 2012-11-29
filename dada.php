4b227a2f95c3eb27050d1817a9c7b9aa31bd6142
mariustoica <mariustoica@gmail.com>
2012-11-29 10:18:25 +0200

    Cleaner code & new AGPL licencing

--- dada.php
+++ dada.php
@@ -1,5 +1,26 @@
 <?php
-
+/**
+ * Dada Scissors - a simple PHP script to extract document fragments for use in your html collage.
+ * Copyright (C) 2012, Dada Scissors contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author   Marius Stoica <mariustoica@gmail.com>
+ *
+ * @license  GNU Affero General Public License http://www.gnu.org/licenses/
+ */
+ 
 //Get the url query string
 $dom = new DOMDocument;
 $dom->loadHTMLfile($_GET['url']);
@@ -9,37 +30,25 @@ $xpath = new DOMXPath($dom);
 $nodes = $xpath->query($_GET['xpath']);
 
 //Extract and display the document fragment! Here xpath query must be absolute path.
-if (preg_match ("/^\/html\/\/body\/(\/.*\/)+\[\d\]$/", $_GET['xpath']) || preg_match ("/^\/html\/\/body\/(\/.*\/)+/", $_GET['xpath'])) {
+if (preg_match ("/^\/html\/body(\/.*)*\/\[\d\]$/", $_GET['xpath']) || preg_match ("/^\/html\/body(\/.*)*$/", $_GET['xpath'])) {	
+	//Remove body and all its children
+	$html = $dom->documentElement;
+	$body_old = $html->getElementsByTagName('body')->item(0);
+	$html->removeChild($body_old);
 	//Fix the urls!
-	$head = $dom->getElementsByTagName('head');
+	$head = $html->getElementsByTagName('head')->item(0);
 	$url_base = $dom->createTextNode('<base href="' . preg_replace('/^(http.+\.\w{2,}\/).*/', '$1', $_GET['url']) . '" />');	
-	foreach($head as $e) {
-		$e->appendChild($url_base);
-	}
-	
-	//Find script and style (TODO) elements inside body
-	$body = $dom->getElementsByTagName('body');
-	foreach($body as $f) {
-		$f->getElementsByTagName('script');
-	}
-	
-	//Build the page!
-	//This can be better: the parents of the selected fragment are not included
-        //so their css inherited by the elements of the selected fragment don't applay to the final page;
-        //the same with the scripts.
-	echo '<html>';
-	echo htmlspecialchars_decode($dom->saveHtml($e));
-	echo '<body>';
-	foreach($nodes as $g) {
-		echo $dom->saveHtml($g);
-	}
-	echo $dom->saveHtml($f);
-	echo '</body>';
-	echo '</html>';
+	$head->insertBefore($url_base, $head->firstChild);
+	//Create new body element that will hold the fragment from xpath
+	$body = $dom->createElement('body');
+	$html->appendChild($body);
+	$body->appendChild($nodes->item(0));
+	//Display the result
+	echo htmlspecialchars_decode($dom->saveHtml());
 }
 
-//Preview the possible document fragments using a simple, relative xpath query for particular
-//elements like <p> or <div> that might hold the fragment you need extracted (something like '//p' will do).
+//Preview the possible document fragments using a simple, relative xpath query for particular elements,
+//like <p> or <div>, that might hold the fragment you need extracted (something like '//p' will do).
 //Copy/paste the absolute xpath and reload the page.
 //From the user point of view this is the first operation to be made.
 else {
