de663e70cd17df0e9f35f61b62b37254533c4948
mariuss <mariustoica@gmail.com>
2012-11-29 10:36:16 +0200

    Update dada.php

--- dada.php
+++ dada.php
@@ -17,7 +17,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @author   Marius Stoica <mariustoica@gmail.com>
- *
  * @license  GNU Affero General Public License http://www.gnu.org/licenses/
  */
  
